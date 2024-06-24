16521be82dd2b062c2255fa7cab26243985312db
Khadija Benali <benalii.2001@gmail.com>
2024-06-24 10:49:06 +0100

    commit

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,10 +1,8 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 @ContextConfiguration
-@SpringBootTest
 class DemoApplicationTests {
 
 	@Test
